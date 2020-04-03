e5faa4bbbc567c475f1a97d4d98929c66b9d6757
Adam <carnagey.adam@gmail.com>
2020-04-03 17:41:34 -0500

    starting on table layout documents

--- src/components/main/MainPage.tsx
+++ src/components/main/MainPage.tsx
@@ -10,8 +10,6 @@ import './MainPage.scss';
 import ShareRequest from '../../models/ShareRequest';
 import classNames from 'classnames';
 import AccountSummary from './account/AccountSummary';
-import DeleteContactBtn from './account/DeleteContactBtn';
-import Toggle from '../common/Toggle';
 
 interface MainPageProps {
   sortAsc: boolean;
@@ -30,23 +28,192 @@ interface MainPageProps {
   removeShareRequest: (request: ShareRequest) => void;
 }
 
-class MainPage extends Component<MainPageProps> {
+interface MainPageState {
+  isLayoutGrid: boolean;
+}
+
+class MainPage extends Component<MainPageProps, MainPageState> {
 
   constructor(props: Readonly<MainPageProps>) {
     super(props);
+    this.state = {
+      isLayoutGrid: true
+    };
   }
+
+  toggleLayout = () => {
+    const {isLayoutGrid} = {...this.state};
+    this.setState({isLayoutGrid: !isLayoutGrid});
+  };
 
   toggleTab = (tab: string) => {
     const {activeTab, setActiveTab} = {...this.props};
     if (activeTab !== tab) setActiveTab(tab);
   };
 
+  renderDocumentGridView() {
+    const {
+      sortAsc, toggleSort, handleAddNew, searchedDocuments,
+      handleSelectDocument, searchedAccounts, shareRequests
+    } = {...this.props};
+    return (
+      <Fragment>
+        <div className="sort-section">
+          <div className="subtitle">Sort by</div>
+          <div className="subtitle subtitle-key" onClick={toggleSort}>NAME</div>
+          <Chevron isAscending={sortAsc} onClick={toggleSort}/>
+        </div>
+        <Row style={{marginRight: '0', minHeight: '480px'}}>
+          <Col
+            sm="12" md="6" lg="4"
+            className="document-add-new document-item"
+            onClick={handleAddNew}
+          >
+            <AddNewDocument handleAddNew={handleAddNew}/>
+          </Col>
+          {searchedDocuments.length <= 0 && (
+            <Col
+              sm="12" md="6" lg="4"
+              className="document-summary-container"
+            >
+              <div style={{display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+                <h4>No Documents.</h4>
+              </div>
+            </Col>
+          )}
+          {searchedDocuments.map((document, idx) => {
+            const sharedAccounts: Account[] = searchedAccounts.filter(sharedAccount => {
+              const matchedShareRequest = shareRequests.find(shareRequest => {
+                return shareRequest.shareWithAccountId === sharedAccount.id
+                  && shareRequest.documentType === document.type;
+              });
+              if (matchedShareRequest) {
+                return sharedAccount;
+              }
+            });
+            return (
+              <Col
+                key={idx}
+                sm="12" md="6" lg="4"
+                className="document-summary-container"
+              >
+                <div
+                  style={{cursor: 'pointer', marginRight: '0'}}
+                  onClick={() => handleSelectDocument(document)}>
+                  <DocumentSummary
+                    document={document}
+                    documentIdx={idx++}
+                    sharedAccounts={sharedAccounts}
+                  />
+                </div>
+              </Col>
+            );
+          })}
+        </Row>
+      </Fragment>
+    );
+  }
+
+  renderDocumentListView() {
+    const {
+      sortAsc, toggleSort, handleAddNew, searchedDocuments,
+      handleSelectDocument, searchedAccounts, shareRequests
+    } = {...this.props};
+    return (
+      <div>
+        <table className="doc-table">
+          <thead>
+          <tr>
+            <th>Name</th>
+            <th>Shared With</th>
+            <th>Uploaded</th>
+            <th>Valid Until</th>
+            <th>Notarized</th>
+          </tr>
+          </thead>
+          <tbody>
+          { searchedDocuments.map((document, idx) => {
+            return (
+              <tr key={document.type}>
+                <td>{document.type}</td>
+                <td>{document.sharedWithAccountIds.length}</td>
+                <td>{document.uploadedBy} {document.updatedAt}</td>
+                <td>{document.validateUntilDate}</td>
+                <td>{document.notarized}</td>
+              </tr>
+            );
+          })}
+          </tbody>
+        </table>
+        {searchedDocuments.length <= 0 && (
+          <Col
+            sm="12"
+            className="document-summary-container"
+          >
+            <div style={{display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
+              <h4>No Documents.</h4>
+            </div>
+          </Col>
+        )}
+      </div>
+    );
+  }
+
+  renderNetworkGridView() {
+    const {
+      sortAsc, toggleSort, searchedDocuments,
+      searchedAccounts, shareRequests,
+      myAccount, addShareRequest, removeShareRequest
+    } = {...this.props};
+    return (
+      <Fragment>
+        <div className="sort-section">
+          <div className="subtitle">Sort by</div>
+          <div className="subtitle subtitle-key" onClick={toggleSort}>NAME</div>
+          <Chevron isAscending={sortAsc} onClick={toggleSort}/>
+        </div>
+        <Row className="network-row">
+          {searchedAccounts.length <= 0 && (
+            <div className="no-network">There are no contacts.</div>
+          )}
+          {searchedAccounts.map(account => {
+              const matchedShareRequests = shareRequests
+                .filter(shareRequest => shareRequest.shareWithAccountId === account.id);
+              return (
+                <Col
+                  key={account.id}
+                  sm="12" md="12" lg="6" xl="4"
+                  className="network-container"
+                >
+                  <AccountSummary
+                    account={account}
+                    shareRequests={matchedShareRequests}
+                    searchedDocuments={searchedDocuments}
+                    myAccount={myAccount}
+                    addShareRequest={addShareRequest}
+                    removeShareRequest={removeShareRequest}
+                  />
+                </Col>
+              );
+            }
+          )}
+        </Row>
+      </Fragment>
+    );
+  }
+
+  renderNetworkListView() {
+    // TODO
+    return this.renderNetworkGridView();
+  }
+
   render() {
     const {
-      sortAsc, toggleSort, handleAddNew, searchedDocuments, activeTab, myAccount,
-      handleSelectDocument, referencedAccount, goBack, searchedAccounts, shareRequests,
+      sortAsc, toggleSort, searchedDocuments, activeTab, myAccount,
+      referencedAccount, goBack, searchedAccounts, shareRequests,
       addShareRequest, removeShareRequest
     } = {...this.props};
+    const {isLayoutGrid} = {...this.state};
     return (
       <div className="main-content">
         {referencedAccount &&
@@ -81,97 +248,19 @@ class MainPage extends Component<MainPageProps> {
               </NavItem>
             </Nav>
             <div className="document-toolbar">
-              <SvgButton buttonType={SvgButtonTypes.LAYOUT_GRID}/>
+              {isLayoutGrid && <SvgButton buttonType={SvgButtonTypes.LAYOUT_GRID} onClick={this.toggleLayout}/>}
+              {!isLayoutGrid && <SvgButton buttonType={SvgButtonTypes.LAYOUT_LIST} onClick={this.toggleLayout}/>}
               <SvgButton buttonType={SvgButtonTypes.INFO}/>
             </div>
           </div>
           <TabContent activeTab={activeTab}>
             <TabPane tabId="1">
-              <div className="sort-section">
-                <div className="subtitle">Sort by</div>
-                <div className="subtitle subtitle-key" onClick={toggleSort}>NAME</div>
-                <Chevron isAscending={sortAsc} onClick={toggleSort}/>
-              </div>
-              <Row style={{marginRight: '0', minHeight: '480px'}}>
-                <Col
-                  sm="12" md="6" lg="4"
-                  className="document-add-new document-item"
-                  onClick={handleAddNew}
-                >
-                  <AddNewDocument handleAddNew={handleAddNew}/>
-                </Col>
-                {searchedDocuments.length <= 0 && (
-                  <Col
-                    sm="12" md="6" lg="4"
-                    className="document-summary-container"
-                  >
-                    <div style={{display: 'flex', height: '100%', justifyContent: 'center', alignItems: 'center'}}>
-                      <h4>No Documents.</h4>
-                    </div>
-                  </Col>
-                )}
-                {searchedDocuments.map((document, idx) => {
-                  const sharedAccounts: Account[] = searchedAccounts.filter(sharedAccount => {
-                    const matchedShareRequest = shareRequests.find(shareRequest => {
-                      return shareRequest.shareWithAccountId === sharedAccount.id
-                        && shareRequest.documentType === document.type;
-                    });
-                    if (matchedShareRequest) {
-                      return sharedAccount;
-                    }
-                  });
-                  return (
-                    <Col
-                      key={idx}
-                      sm="12" md="6" lg="4"
-                      className="document-summary-container"
-                    >
-                      <div
-                        style={{cursor: 'pointer', marginRight: '0'}}
-                        onClick={() => handleSelectDocument(document)}>
-                        <DocumentSummary
-                          document={document}
-                          documentIdx={idx++}
-                          sharedAccounts={sharedAccounts}
-                        />
-                      </div>
-                    </Col>
-                  );
-                })}
-              </Row>
+              {isLayoutGrid && this.renderDocumentGridView()}
+              {!isLayoutGrid && this.renderDocumentListView()}
             </TabPane>
             <TabPane tabId="2">
-              <div className="sort-section">
-                <div className="subtitle">Sort by</div>
-                <div className="subtitle subtitle-key" onClick={toggleSort}>NAME</div>
-                <Chevron isAscending={sortAsc} onClick={toggleSort}/>
-              </div>
-              <Row className="network-row">
-                {searchedAccounts.length <= 0 && (
-                  <div className="no-network">There are no contacts.</div>
-                )}
-                {searchedAccounts.map(account => {
-                  const matchedShareRequests = shareRequests
-                    .filter(shareRequest => shareRequest.shareWithAccountId === account.id);
-                  return (
-                    <Col
-                      key={account.id}
-                      sm="12" md="12" lg="6" xl="4"
-                      className="network-container"
-                    >
-                      <AccountSummary
-                        account={account}
-                        shareRequests={matchedShareRequests}
-                        searchedDocuments={searchedDocuments}
-                        myAccount={myAccount}
-                        addShareRequest={addShareRequest}
-                        removeShareRequest={removeShareRequest}
-                      />
-                    </Col>
-                  );
-                  }
-                )}
-              </Row>
+              {isLayoutGrid && this.renderNetworkGridView()}
+              {!isLayoutGrid && this.renderNetworkListView()}
             </TabPane>
           </TabContent>
         </Fragment>
