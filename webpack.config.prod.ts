9471037d56c3f1913cbffb027229174209601191
Adam Carnagey <carnagey.adam@gmail.com>
2020-10-14 12:53:17 -0500

    hotjar account change, troubleshooting

--- webpack.config.prod.ts
+++ webpack.config.prod.ts
@@ -31,11 +31,11 @@ const config: webpack.Configuration = {
       template: './src/index.html',
       filename: './index.html',
       favicon: 'public/favicon.ico',
-      hotjar: `<!-- Hotjar Tracking Code for https://mypass-atx.s3.us-east-2.amazonaws.com/inde -->
+      hotjar: `<!-- Hotjar Tracking Code for https://mypass-atx.s3.us-east-2.amazonaws.com/index.html -->
       <script>
           (function(h,o,t,j,a,r){
               h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-              h._hjSettings={hjid:2025647,hjsv:6};
+              h._hjSettings={hjid:2041416,hjsv:6};
               a=o.getElementsByTagName('head')[0];
               r=o.createElement('script');r.async=1;
               r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
